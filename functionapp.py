0dd67cb9c26b0f949f57c23e72dfa5b340948c4d
orgdevendra <65060947+orgdevendra@users.noreply.github.com>
2025-03-19 20:38:49 +0530

    Update functionapp.py

--- functionapp.py
+++ functionapp.py
@@ -1,22 +1,13 @@
-import os
 import logging
-from azure.servicebus import ServiceBusClient, ServiceBusMessage
-from azure.webpubsub import WebPubSubServiceClient
 import azure.functions as func
-
-# Azure Service Bus connection details
-SERVICE_BUS_CONNECTION_STR = os.getenv('SERVICE_BUS_CONNECTION_STR')
-WEATHER_TOPIC = 'weather-topic'
-WIND_TOPIC = 'wind-topic'
-
-# Azure Web PubSub connection details
-WEBPUBSUB_CONNECTION_STR = os.getenv('WEBPUBSUB_CONNECTION_STR')
-HUB_NAME = 'notificationhub'
-
-# Initialize Web PubSub client
-webpubsub_client = WebPubSubServiceClient.from_connection_string(WEBPUBSUB_CONNECTION_STR, hub=HUB_NAME)
+from azure.webpubsub import WebPubSubServiceClient
 
 def main(msg: func.ServiceBusMessage):
-    logging.info(f'Received message: {msg.get_body().decode("utf-8")}')
+    logging.info('Python ServiceBus topic trigger function processed a message: %s', msg.get_body().decode('utf-8'))
+
+    # Initialize Web PubSub client
+    webpubsub_client = WebPubSubServiceClient.from_connection_string("WEBPUBSUB_CONNECTION_STR")
+
     # Send message to Web PubSub
-    webpubsub_client.send_to_all(msg.get_body().decode("utf-8"))
+    message = msg.get_body().decode('utf-8')
+    webpubsub_client.send_to_all(message)
